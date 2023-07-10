83c6fa093cf1394aa6bf6b486bb1e9337193b61c
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-10 17:15:10 -0300

    model
    User Model

--- api/api/models.py
+++ api/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserModel(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "users"
