36e467705e4c90bbc1d39adc5f92fdc00cea1aa7
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-11 10:20:33 -0300

    Models
    Letter Model

--- api/api/models.py
+++ api/api/models.py
@@ -9,3 +9,13 @@ class UserModel(models.Model):
 
     class Meta:
         verbose_name_plural = "users"
+
+
+class LetterModel(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    date = models.DateField()
+    text = models.CharField(max_length=5000)
+    sender = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "letters"
