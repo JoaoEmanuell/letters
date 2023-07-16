9d413bf0803ecc091180c10f78db9b19f8d329dc
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-16 15:14:40 -0300

    tests: user
    User tests
    
    get all, crud, login

--- api/api/tests.py
+++ api/api/tests.py
@@ -1,34 +1,98 @@
 from django.test import TestCase
 
-from api.models import LetterModel, UserModel
-from datetime import date
+from requests import get, post, put, delete
 
 # Create your tests here.
 
 
-class UserModelTestCase(TestCase):
+class UserTest(TestCase):
     def setUp(self):
-        UserModel.objects.create(name="test", password="test")
+        self.__url = "http://localhost:8000/api/user"
 
-    def test_get_user(self):
-        test_user = UserModel.objects.get(name="test")
-        self.assertEqual(test_user.password, "test")
+    def test_get_all_users(self):
+        response = get(self.__url)
+        status_code = response.status_code
+        json = response.json()
 
+        self.assertEqual(status_code, 401)
+        self.assertEqual(
+            json, {"detail": "Authentication credentials were not provided."}
+        )
 
-class LetterModelTestCase(TestCase):
-    def setUp(self):
-        UserModel.objects.create(name="test", password="test")
+    def test_crud(self):
+        # Create
+        data = {"name": "test", "username": "test", "password": "test"}
+        response = post(self.__url, data=data)
 
-    def test_create_letter(self):
-        user = UserModel.objects.get(name="test")
-        letter_dict = {
-            "user": user,
-            "date": date(2005, 7, 27),
-            "text": "Test letter",
-            "sender": "Anonymous",
-        }
-        LetterModel.objects.create(**letter_dict)
-        letter = LetterModel.objects.get(sender="Anonymous")
-        self.assertEqual(letter.text, "Test letter")
-        self.assertEqual(letter.id, 1)
-        self.assertEqual(letter.user, user)
+        status_code = response.status_code
+        json = response.json()
+        new_data = json
+
+        token = new_data["token"]
+
+        self.assertEqual(status_code, 201)
+        self.assertEqual(data["username"], json["username"])
+
+        # Read
+        detail_url = f"{self.__url}/detail/{token}"
+        response = get(detail_url)
+        status_code = response.status_code
+        json = response.json()
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(json, new_data)
+
+        # Put
+        data_put = data
+        data_put["name"] = "abc"
+
+        response = put(detail_url, data=data_put)
+        status_code = response.status_code
+        json = response.json()
+        self.assertEqual(status_code, 200)
+        self.assertEqual(json["name"], data_put["name"])
+
+        # Delete
+        response = delete(detail_url)
+        status_code = response.status_code
+        json = response.json()
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(json, {"res": "User deleted!"})
+
+    def test_login(self):
+        # Create
+        data = {"name": "test", "username": "test", "password": "test"}
+        response = post(self.__url, data=data)
+        status_code = response.status_code
+        self.assertEqual(status_code, 201)
+
+        json = response.json()
+        token = json["token"]
+
+        # Login
+        data_login = {"username": "test", "password": "test"}
+        response = post(f"{self.__url}/login", data=data_login)
+
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+        json = response.json()
+        token_login = json["res"]
+        self.assertEqual(token, token_login)
+
+        # Invalid password
+        data_login["password"] = "abc"
+        response = post(f"{self.__url}/login", data=data_login)
+
+        status_code = response.status_code
+        self.assertEqual(status_code, 400)
+
+        json = response.json()
+        self.assertEqual({"res": "Invalid password"}, json)
+
+        # Delete
+        response = delete(f"{self.__url}/detail/{token_login}")
+
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
