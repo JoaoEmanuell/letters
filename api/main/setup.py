9eb2464b9489ba88ce4d741d850d6139e2b3f104
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-16 17:11:28 -0300

    feat: fernet
    Fernet to cryptograph letters

--- api/main/setup.py
+++ api/main/setup.py
@@ -4,7 +4,14 @@ def generate_token_hex() -> str:
     return token_hex(32)
 
 
+def generate_fernet_key() -> bytes:
+    from cryptography.fernet import Fernet
+
+    return Fernet.generate_key()
+
+
 if __name__ == "__main__":
+    # Cryptograph to models
     cryptograph_key = []
     for _ in range(8):  # Number of keys to generate, used to models
         cryptograph_key.append(generate_token_hex())
@@ -12,6 +19,12 @@ if __name__ == "__main__":
     cryptograph_str = (
         f"\n\n# Encrypted fields\n\nFIELD_ENCRYPTION_KEYS = {cryptograph_key}\n"
     )
+
+    with open("settings.py", "a") as file:
+        file.write(cryptograph_str)
+
+    # Cryptograph to letters
+    cryptograph_str = f"\n\n# Fernet key\n\nFERNET_KEY = {generate_fernet_key()}\n"
 
     with open("settings.py", "a") as file:
         file.write(cryptograph_str)
--- api/main/utils.py
+++ api/main/utils.py
@@ -1,7 +1,10 @@
 from bcrypt import hashpw, gensalt, checkpw
+from cryptography.fernet import Fernet
 
+from .settings import FERNET_KEY
 
 ENCODE = "utf-8"
+fernet = Fernet(FERNET_KEY)
 
 
 def generate_hash(value: str) -> str:
@@ -15,3 +18,13 @@ def compare_hash(value: str, hash: str) -> bool:
     if type(hash) != bytes:
         hash = bytes(hash, encoding=ENCODE)
     return checkpw(bytes(value, encoding=ENCODE), hash)
+
+
+def cryptograph_text(text: str) -> bytes:
+    global fernet
+    return fernet.encrypt(text.encode("utf-8"))
+
+
+def decrypt_text(text: str) -> str:
+    global fernet
+    return fernet.decrypt(text).decode()
