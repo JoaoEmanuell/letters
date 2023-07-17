e2c512440b2d6c9387f3929553bfac938cb2ca04
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-17 12:00:56 -0300

    test: LetterTest

--- api/api/tests.py
+++ api/api/tests.py
@@ -96,3 +96,79 @@ class UserTest(TestCase):
 
         status_code = response.status_code
         self.assertEqual(status_code, 200)
+
+
+class LetterTest(TestCase):
+    def setUp(self):
+        self.__url = "http://localhost:8000/api/letter"
+
+    def create_user(self) -> str:
+        url = "http://localhost:8000/api/user"
+        data = {"name": "test", "username": "test", "password": "test"}
+        response = post(url, data=data)
+
+        return response.json()["token"]
+
+    def delete_user(self, token: str):
+        url = "http://localhost:8000/api/user"
+        response = delete(f"{url}/detail/{token}")
+
+    def test_get_all_letters(self):
+        response = get(self.__url)
+        status_code = response.status_code
+        json = response.json()
+
+        self.assertEqual(status_code, 401)
+        self.assertEqual(
+            json, {"detail": "Authentication credentials were not provided."}
+        )
+
+    def test_crud(self):
+        # User create for get token
+        user_token = self.create_user()
+
+        # Create
+        data = {
+            "user_token": user_token,
+            "date": "01-01-2000",
+            "sender": "Anonymous",
+            "text": "ABC",
+        }
+
+        response = post(self.__url, data=data)
+        json = response.json()
+        letter_token = json["letter_token"]
+
+        self.assertEqual(data["user_token"], json["user_token"])
+
+        # GET
+        response = get(f"{self.__url}/detail/{letter_token}")
+        json = response.json()
+
+        self.assertEqual(data["text"], json["text"])
+        self.assertEqual(data["sender"], json["sender"])
+
+        # Delete letter
+        response = delete(f"{self.__url}/detail/{letter_token}")
+        json = response.json()
+
+        self.assertEqual({"res": "Letter deleted!"}, json)
+
+        # Create a new
+
+        response = post(self.__url, data=data)
+
+        # Get all letters from user
+        response = get(f"{self.__url}/user/{user_token}")
+        json = response.json()
+
+        self.assertEqual(json[0]["text"], data["text"])
+
+        # Delete all letters
+        response = delete(f"{self.__url}/user/{user_token}")
+        json = response.json()
+
+        self.assertEqual({"res": "Letters deleted!"}, json)
+
+        # Delete user
+        self.delete_user(user_token)
