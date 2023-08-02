5f0ee2afaa280be1faa7304f6c22136e72066c26
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-08-02 11:33:10 -0300

    env example

--- api/main/setup.py
+++ api/main/setup.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 def generate_token_hex() -> str:
     from secrets import token_hex
 
@@ -13,9 +18,8 @@ def generate_fernet_key() -> bytes:
 def create_letters_dir() -> None:
     from os import mkdir
     from os.path import exists
-    from pathlib import Path
 
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    global BASE_DIR
 
     path_to_letters = f"{BASE_DIR}/database/letters/"
     if not exists(path_to_letters):
@@ -25,12 +29,58 @@ def create_letters_dir() -> None:
 def copy_settings() -> None:
     from os.path import exists
 
-    if not exists("settings.py"):
+    global BASE_DIR
+
+    if not exists(f"{BASE_DIR}/database/backup/settings.py"):
         with open("settings_example.py", "r") as settings_example:
             settings_example = settings_example.read()
             with open("settings.py", "w") as settings:
                 settings_with_secret_key = _write_django_secret_key(settings_example)
                 settings.write(settings_with_secret_key)
+                # Cryptograph to models
+                cryptograph_key = []
+                for _ in range(9):  # Number of keys to generate, used to models
+                    cryptograph_key.append(generate_token_hex())
+
+                cryptograph_str = f"\n\n# Encrypted fields\n\nFIELD_ENCRYPTION_KEYS = {cryptograph_key}\n"
+
+                settings.write(cryptograph_str)
+
+                # Cryptograph to letters
+                cryptograph_str = (
+                    f"\n\n# Fernet key\n\nFERNET_KEY = {generate_fernet_key()}\n"
+                )
+
+                settings.write(cryptograph_str)
+
+        backup_settings()
+    else:
+        restore_settings()
+
+
+def backup_settings() -> None:
+    from os.path import exists
+    from os import mkdir
+
+    global BASE_DIR
+
+    backup_dir = f"{BASE_DIR}/database/backup"
+
+    if not exists(backup_dir):
+        mkdir(backup_dir)
+
+    with open("settings.py", "r") as settings:
+        with open(f"{backup_dir}/settings.py", "w") as settings_backup:
+            settings_backup.write(settings.read())
+
+
+def restore_settings() -> None:
+    global BASE_DIR
+
+    backup_dir = f"{BASE_DIR}/database/backup"
+    with open(f"{backup_dir}/settings.py", "r") as settings_backup:
+        with open("settings.py", "w") as settings:
+            settings.write(settings_backup.read())
 
 
 def _write_django_secret_key(settings: str) -> str:
@@ -47,9 +97,8 @@ def _write_django_secret_key(settings: str) -> str:
 def create_database() -> None:
     from os import mkdir
     from os.path import exists
-    from pathlib import Path
 
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    global BASE_DIR
 
     path_to_database = f"{BASE_DIR}/database/"
     # Create database dir
@@ -73,21 +122,3 @@ if __name__ == "__main__":
 
     # Copy settings
     copy_settings()
-
-    # Cryptograph to models
-    cryptograph_key = []
-    for _ in range(9):  # Number of keys to generate, used to models
-        cryptograph_key.append(generate_token_hex())
-
-    cryptograph_str = (
-        f"\n\n# Encrypted fields\n\nFIELD_ENCRYPTION_KEYS = {cryptograph_key}\n"
-    )
-
-    with open("settings.py", "a") as file:
-        file.write(cryptograph_str)
-
-    # Cryptograph to letters
-    cryptograph_str = f"\n\n# Fernet key\n\nFERNET_KEY = {generate_fernet_key()}\n"
-
-    with open("settings.py", "a") as file:
-        file.write(cryptograph_str)
