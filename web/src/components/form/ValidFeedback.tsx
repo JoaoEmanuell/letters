48f6bf29952b69d7883e998252f41b416152821c
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-08-07 17:27:09 -0300

    feat: valid feedback

---
+++ web/src/components/form/ValidFeedback.tsx
@@ -0,0 +1,11 @@
+interface ValidFeedbackInterface {
+    text: string
+}
+
+export function ValidFeedback(props: ValidFeedbackInterface){
+    return (
+        <div className="valid-feedback">
+            {props.text}
+        </div>
+    )
+}
