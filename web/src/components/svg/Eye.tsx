7853cf84bf877284b5204009efb76c37503c9648
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-08-13 10:12:25 -0300

    feat: eye
    open and close event

--- web/src/components/svg/Eye.tsx
+++ web/src/components/svg/Eye.tsx
@@ -4,16 +4,36 @@ import eyeOpen from "../../assets/eye-open.svg"
 import eyeSlash from "../../assets/eye-slash.svg"
 import { useState } from "react"
 
-export function Eye(){
+interface EyeInterface {
+    imageClickedEventOpen?: Function,
+    imageClickedEventClose?: Function
+}
+
+export function Eye(props: EyeInterface){
+    const imageClickedEventOpen = props.imageClickedEventOpen
+    const imageClickedEventClose = props.imageClickedEventClose
     const [imageUrl, setImageUrl] = useState(eyeSlash)
-    const imageClick = () => {
-        if (imageUrl == eyeOpen) {
-            setImageUrl(eyeSlash)
-        } else {
-            setImageUrl(eyeOpen)
+    const [altText, setAltText] = useState("eye-closed")
+
+    const callClickedEvent = (event: Function | undefined) => {
+        if (event != undefined) {
+            event()
         }
     }
+
+    const imageClick = () => {
+        if (imageUrl == eyeOpen) {
+            callClickedEvent(imageClickedEventClose)
+            setImageUrl(eyeSlash)
+            setAltText("eye-closed")
+        } else {
+            callClickedEvent(imageClickedEventOpen)
+            setImageUrl(eyeOpen)
+            setAltText("eye-open")
+        }
+    }
+
     return (
-        <Image src={imageUrl} alt="eye-icon" onClick={imageClick}></Image>
+        <Image src={imageUrl} alt={altText} onClick={imageClick}></Image>
     )
 }
