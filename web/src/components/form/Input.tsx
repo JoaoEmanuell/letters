31dfd31140659313c17f5bc2b3f8dc721cf3c5d6
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-08-13 10:13:11 -0300

    feat: Password Input

--- web/src/components/form/Input.tsx
+++ web/src/components/form/Input.tsx
@@ -2,11 +2,12 @@ interface InputInterface {
     type: string,
     name: string,
     id: string,
-    required?: boolean
+    required?: boolean,
+    maxLength?: number
 }
 
 export function Input(props: InputInterface) {
     return (
-        <input type={props.type} name={props.name} id={props.id} required={props.required} className="form-control"/>
+        <input type={props.type} name={props.name} id={props.id} required={props.required} maxLength={props.maxLength} className="form-control"/>
     )
 }
---
+++ web/src/components/form/PasswordInput.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { useState } from "react"
+import { Eye } from "../svg/Eye"
+import { Input } from "./Input"
+import { InvalidFeedback } from "./InvalidFeedback"
+import { ValidFeedback } from "./ValidFeedback"
+
+export function PasswordInput(){
+    const [inputType, setInputType] = useState("password")
+    const imageClickOpen = () => {
+        setInputType("text")
+    }
+    const imageClickClose = () => {
+        setInputType("password")
+    }
+    return (
+        <div className="input-group has-validation">
+            <span className="input-group-text">
+                <Eye imageClickedEventOpen={imageClickOpen} imageClickedEventClose={imageClickClose}/>
+            </span>
+            <Input type={inputType} name="password" id="password" required={true}/>
+            <InvalidFeedback text="Senha inválida!"/>
+            <ValidFeedback text="Senha válida!"/>
+        </div>
+    )
+}
