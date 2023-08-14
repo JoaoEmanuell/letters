670f512ccc24eee21a92bc6381b61a37afbfba8c
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-08-14 17:25:53 -0300

    feat: Form field component

---
+++ web/src/components/form/FormField.tsx
@@ -0,0 +1,30 @@
+import { Input } from './Input'
+import { InvalidFeedback } from './InvalidFeedback'
+import { Label } from './Label'
+import { ValidFeedback } from './ValidFeedback'
+
+interface FormFieldInterface {
+    labelText: string
+    inputType: string
+    inputId: string
+    inputMaxLength: number
+    invalidFeedbackMessage: string
+    validFeedbackMessage: string
+}
+
+export function FormField(props: FormFieldInterface) {
+    return (
+        <div className="mt-3">
+            <Label for={props.inputId} text={props.labelText} />
+            <Input
+                type={props.inputType}
+                name={props.inputId}
+                id={props.inputId}
+                required={true}
+                maxLength={props.inputMaxLength}
+            />
+            <InvalidFeedback text={props.invalidFeedbackMessage} />
+            <ValidFeedback text={props.validFeedbackMessage} />
+        </div>
+    )
+}
