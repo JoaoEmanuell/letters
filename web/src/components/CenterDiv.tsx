67a10a465769e10e577ce0e78a4ff8c65df8bb77
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-08-22 17:37:49 -0300

    refactor: Center div
    Add mb-4

--- web/src/components/CenterDiv.tsx
+++ web/src/components/CenterDiv.tsx
@@ -1,3 +1,3 @@
 export function CenterDiv({ children }: { children: React.ReactNode }) {
-    return <div className="d-flex justify-content-center">{children}</div>
+    return <div className="d-flex justify-content-center mb-4">{children}</div>
 }
