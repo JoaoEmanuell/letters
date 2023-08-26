8bd5bd6b05de19c1e4a140b12c3b243be7c692f1
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-08-26 17:09:46 -0300

    refactor: submit button
    Add on click

--- web/src/components/form/SubmitButton.tsx
+++ web/src/components/form/SubmitButton.tsx
@@ -1,10 +1,20 @@
 interface SubmitButtonInterface {
     text: string
+    onClick?: Function
 }
 
 export function SubmitButton(props: SubmitButtonInterface) {
+    const onClickCallEvent = () => {
+        if (props.onClick != undefined) {
+            props.onClick()
+        }
+    }
     return (
-        <button type="submit" className="btn btn-primary">
+        <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={onClickCallEvent}
+        >
             {props.text}
         </button>
     )
