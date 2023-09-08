a5deca9f8765210f9ca7fb48f91fc6a2e9ac41f5
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-09-08 11:34:24 -0300

    refactor: center div
    add props

--- web/src/components/body/CenterDiv.tsx
+++ web/src/components/body/CenterDiv.tsx
@@ -1,3 +1,10 @@
-export function CenterDiv({ children }: { children: React.ReactNode }) {
-    return <div className="d-flex justify-content-center mb-4">{children}</div>
+import { PropsWithChildren } from 'react'
+
+interface CenterDivInterface {
+    className?: string
+}
+
+export function CenterDiv(props: PropsWithChildren<CenterDivInterface>) {
+    const className = `d-flex justify-content-center mb-4 ${props.className}`
+    return <div className={className}>{props.children}</div>
 }
