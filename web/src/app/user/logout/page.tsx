e984f4f3535ef413d2f997fe8ca3d9758fa159c9
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-09-08 11:33:55 -0300

    logout page

---
+++ web/src/app/user/logout/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { CenterDiv } from '@/components/body/CenterDiv'
+import { GetCookie } from '@/functions/cookies/GetCookie'
+import { RemoveCookie } from '@/functions/cookies/RemoveCookie'
+import { SetCookie } from '@/functions/cookies/SetCookie'
+
+export default function UserLogout() {
+    const logout = () => {
+        const cookiesName = ['name', 'username', 'userToken']
+        cookiesName.forEach((cookieName) => {
+            RemoveCookie(cookieName)
+        })
+        SetCookie('flash', 'success+Saída realizada com sucesso!') // Flash message
+        window.location.replace('/')
+    }
+    // Validate if user is not authenticated
+    if (GetCookie('userToken') === '') {
+        window.location.replace(`/`)
+    }
+    return (
+        <main>
+            <h1 className="text-center">Deseja sair da aplicação?</h1>
+            <div className="container mt-2">
+                <CenterDiv className="p-4">
+                    <button
+                        type="button"
+                        className="btn btn-danger ms-1"
+                        onClick={logout}
+                    >
+                        Sim
+                    </button>
+                    <a className="btn btn-success ms-1" href="/">
+                        Não
+                    </a>
+                </CenterDiv>
+            </div>
+        </main>
+    )
+}
