ecf64dd0e0b20176f664e97825d690518e135746
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-11-03 12:22:56 -0300

    fix: cookies document error

--- web/src/functions/cookies/GetCookie.tsx
+++ web/src/functions/cookies/GetCookie.tsx
@@ -1,14 +1,18 @@
 export function GetCookie(cookieName: string) {
-    const name = `${cookieName}`
-    const cookies = document.cookie.split(';')
-    var cookieValue = ''
+    try {
+        const name = `${cookieName}`
+        const cookies = document.cookie.split(';')
+        var cookieValue = ''
 
-    cookies.forEach((cookie) => {
-        const cookieKey = cookie.split('=')[0].trim()
-        if (cookieKey == name) {
-            cookieValue = cookie.split('=')[1].trim()
-        }
-    })
+        cookies.forEach((cookie) => {
+            const cookieKey = cookie.split('=')[0].trim()
+            if (cookieKey == name) {
+                cookieValue = cookie.split('=')[1].trim()
+            }
+        })
 
-    return decodeURIComponent(cookieValue)
+        return decodeURIComponent(cookieValue)
+    } catch (err) {
+        return ''
+    }
 }
--- web/src/functions/cookies/RemoveCookie.tsx
+++ web/src/functions/cookies/RemoveCookie.tsx
@@ -1,3 +1,5 @@
 export function RemoveCookie(name: string) {
-    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`
+    try {
+        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`
+    } catch {}
 }
